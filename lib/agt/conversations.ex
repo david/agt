c918b3a06a96a6e3c786c856f12f6a383dd42e6b
David Leal <david@davidleal.com>
2025-06-17 16:14:32 +0100

    Try to generate a unique id for a new message

--- lib/agt/conversations.ex
+++ lib/agt/conversations.ex
@@ -4,18 +4,43 @@ defmodule Agt.Conversations do
   """
 
   def create_message(message, conversation_id) do
+    iodata = JSON.encode_to_iodata!(message)
+
     File.mkdir_p!("conversations/#{conversation_id}")
 
+    write_message(iodata, conversation_id)
+
+    {:ok, message}
+  end
+
+  defp write_message(iodata, conversation_id) do
     timestamp = DateTime.utc_now() |> DateTime.to_unix()
-    path = "conversations/#{conversation_id}/#{timestamp}.json"
+
+    write_message(iodata, conversation_id, timestamp)
+  end
+
+  defp write_message(iodata, conversation_id, timestamp, retry_number \\ 0) do
+    counter = retry_number |> to_string() |> String.pad_leading(2, "0")
+    message_id = "#{timestamp}#{counter}"
+
+    case write_file(iodata, conversation_id, message_id) do
+      :ok ->
+        :ok
+
+      {:error_exists, error} ->
+        IO.puts(error)
+
+        write_message(iodata, conversation_id, timestamp, retry_number + 1)
+    end
+  end
+
+  defp write_file(message, conversation_id, message_id) do
+    path = "conversations/#{conversation_id}/#{message_id}.json"
 
     if File.exists?(path) do
-      {:error, "Message exists: #{path}"}
+      {:error_exists, "Message file exists: #{path}"}
     else
-      iodata = JSON.encode_to_iodata!(message)
-      :ok = File.write(path, iodata)
-
-      {:ok, message}
+      File.write(path, message)
     end
   end
 end
