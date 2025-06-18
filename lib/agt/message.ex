2ad0fbed696ff8c9da80e74f36994cd8394af45c
David Leal <david@davidleal.com>
2025-06-18 10:07:17 +0100

    Store message roles.

--- lib/agt/message.ex
+++ lib/agt/message.ex
@@ -1,21 +1,21 @@
 defmodule Agt.Message do
   defmodule Prompt do
-    @derive {JSON.Encoder, only: [:body]}
-    defstruct [:body]
+    @derive {JSON.Encoder, only: [:body, :role]}
+    defstruct [:body, role: "user"]
   end
 
   defmodule Response do
-    @derive {JSON.Encoder, only: [:body]}
-    defstruct [:body]
+    @derive {JSON.Encoder, only: [:body, :role]}
+    defstruct [:body, role: "model"]
   end
 
   defmodule FunctionCall do
-    @derive {JSON.Encoder, only: [:name, :arguments]}
-    defstruct [:name, :arguments]
+    @derive {JSON.Encoder, only: [:name, :arguments, :role]}
+    defstruct [:name, :arguments, role: "model"]
   end
 
   defmodule FunctionResponse do
-    @derive {JSON.Encoder, only: [:name, :result]}
-    defstruct [:name, :result]
+    @derive {JSON.Encoder, only: [:name, :result, :role]}
+    defstruct [:name, :result, role: "user"]
   end
 end
